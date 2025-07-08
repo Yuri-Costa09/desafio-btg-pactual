543ec11d018a1eaef0428768488eb46c590664c9
Yuri-Costa09 <yurircostawork@gmail.com>
2025-07-08 18:55:14 -0300

    feat: Base entities made, Entities mapped to mongoDB, basic config of RabbitMQ

---
+++ order-service/src/main/java/com/yuricosta/btg/pactual/order_service/config/RabbitmqConfig.java
@@ -0,0 +1,8 @@
+package com.yuricosta.btg.pactual.order_service.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+}
