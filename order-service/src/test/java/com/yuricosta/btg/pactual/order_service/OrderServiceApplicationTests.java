bcdc940016b5ccdc89a532c929eca94254814b62
Yuri-Costa09 <yurircostawork@gmail.com>
2025-07-08 18:20:09 -0300

    initialize project structure with Maven and Spring Boot setup

---
+++ order-service/src/test/java/com/yuricosta/btg/pactual/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuricosta.btg.pactual.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
